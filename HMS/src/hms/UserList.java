276176533c7a8c6409fa1a5723cd9301d3d66e02
Rin <82919982+Rin-chan@users.noreply.github.com>
2024-10-28 01:43:23 +0800

    Improved Javadocs

--- HMS/src/hms/UserList.java
+++ HMS/src/hms/UserList.java
@@ -1,88 +1,134 @@
 package hms;
 
 import hms.users.Patient;
+import hms.users.Doctor;
+import hms.users.Pharmacist;
+import hms.users.Administrator;
+
 import java.util.ArrayList;
 
 public class UserList {
     /**
      * List of Doctors in the system
      */
-    private ArrayList<Doctor> doctors;
+    private ArrayList<Doctor> doctors = new ArrayList<Doctor>();
 
     /**
      * List of Patients in the system
      */
-    private ArrayList<Patient> patients;
+    private ArrayList<Patient> patients = new ArrayList<Patient>();
 
     /**
      * List of Pharmacist in the system
      */
-    private ArrayList<Pharmacist> pharmacists;
+    private ArrayList<Pharmacist> pharmacists = new ArrayList<Pharmacist>();
 
     /**
      * List of Administrator in the system
      */
-    private ArrayList<Administrator> adminstrators;
+    private ArrayList<Administrator> administrators = new ArrayList<Administrator>();
 
     /**
-     * Get/Set/Add Doctors
+     * Constructor
+     */
+    public UserList() {
+    	
+    }
+    
+    /**
+     * Accessor of Doctors
+     * @return
      */
     public ArrayList<Doctor> getDoctors() {
         return doctors;
     }   
 
-    public void setDoctors(Doctor doctors) {
+    /**
+     * Setter of Doctors
+     * @param doctors
+     */
+    public void setDoctors(ArrayList<Doctor> doctors) {
         this.doctors = doctors;
     }
 
+    /**
+     * Add Doctor
+     * @param doctor
+     */
     public void addDoctor(Doctor doctor) {
         doctors.add(doctor);
     }
 
     /**
-     * Get/Set/Add Patients
+     * Accessor of Patients
+     * @return list of Patients
      */
     public ArrayList<Patient> getPatients() {
         return patients;
     }
 
-    public void setPatients(Patient patients) {
+    /**
+     * Setter of Patients
+     * @param patients
+     */
+    public void setPatients(ArrayList<Patient> patients) {
         this.patients = patients;
     }
 
+    /**
+     * Add Patient
+     * @param patient
+     */
     public void addPatient(Patient patient) {
         patients.add(patient);
     }
 
 
     /**
-     * Get/Set/Add Pharmacist
+     * Accessor of Pharmacist
+     * @return list of Pharamacists
      */
     public ArrayList<Pharmacist> getPharmacist() {
         return pharmacists;
     }
 
-    public void setPharmacist(Pharmacist pharmacists) {
+    /**
+     * Setter of Administrators
+     * @param pharmacists
+     */
+    public void setPharmacist(ArrayList<Pharmacist> pharmacists) {
         this.pharmacists = pharmacists;
     }
-
+    
+    /**
+     * Add Pharmacist
+     * @param pharmacist
+     */
     public void addPharmacist(Pharmacist pharmacist) {
         pharmacists.add(pharmacist);
     }
 
     /**
-     * Get/Set/Add Administrators
+     * Accesspr of Administrators
+     * @return list of administrators
      */
-
     public ArrayList<Administrator> getAdministrator() {
-        return adminstrators;
+        return administrators;
     }
 
-    public void setAdministrator(Administrator administrators) {
+    /**
+     * Setter of Administrators
+     * @param administrators
+     */
+    public void setAdministrator(ArrayList<Administrator> administrators) {
         this.administrators = administrators;
     }
 
-    public void addAdministrator(Administrato administrator) {
+    /**
+     * Add Administrators
+     * @param administrator
+     */
+    public void addAdministrator(Administrator administrator) {
         administrators.add(administrator);
     }
 
