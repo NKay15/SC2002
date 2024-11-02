2455e6bc8f3bda160762ef59475d16548085ee32
Rin <82919982+Rin-chan@users.noreply.github.com>
2024-11-02 19:39:49 +0800

    Constructor for Adminstrator and Pharmacist

--- HMS/src/hms/users/Administrator.java
+++ HMS/src/hms/users/Administrator.java
@@ -3,4 +3,11 @@ package hms.users;
 import hms.users.*;
 
 public class Administrator extends Staff {
+	
+	/**
+	 * Constructor
+	 */
+	public Administrator(String ID, String name, int gender, int age) {
+		super(ID, name, 4, gender, age);
+	}
 }
